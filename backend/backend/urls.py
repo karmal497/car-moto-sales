67235c691728516348833e33b80ba5eef3311aca
karmal <karmal1711pro@gmail.com>
2025-12-19 13:52:37 -0800

    guardar

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -12,14 +12,14 @@ urlpatterns = [
 
 # ¡IMPORTANTE! Quita el if settings.DEBUG:
 # Sirve archivos media en desarrollo Y producción
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # OPCIONAL: Para producción también puedes usar esta alternativa:
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# else:
-#     urlpatterns += [
-#         re_path(r'^media/(?P<path>.*)$', serve, {
-#             'document_root': settings.MEDIA_ROOT,
-#         }),
-#     ]
+if settings.DEBUG:
+     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+     urlpatterns += [
+         re_path(r'^media/(?P<path>.*)$', serve, {
+             'document_root': settings.MEDIA_ROOT,
+         }),
+     ]
