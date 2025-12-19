7ace94425b06bddd5be2c2bf5e415c92b55cf40e
karmal <karmal1711pro@gmail.com>
2025-12-19 06:23:27 -0800

    guardar

---
+++ frontend-admin/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  // DEBE apuntar a tu backend en Render, NO a localhost
+  apiUrl: 'https://webvehicles-backend.onrender.com/api'
+};
---
+++ frontend-admin/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  // Para desarrollo local puedes dejar localhost
+  apiUrl: 'http://localhost:8000/api'
+};
